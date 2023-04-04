1b7626b9ea17fc7c7fd078a23b2aadfbaaf5b19e
Andrej <antandrej2003@gmail.com>
2023-04-05 00:34:21 +0200

    Adding forums

--- test/src/app/forums/forums.component.ts
+++ test/src/app/forums/forums.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import forumsData from "../forums.json";
+
+interface Forums{
+  forumName: string;
+  time: string;
+  text: string;
+  listeners: number;
+  picture: string;
+  moderatorName: string;
+  moderatorDesc: string;
+  title: string;
+}
 
 @Component({
-  selector: 'forum',
+  selector: 'forums',
   templateUrl: './forums.component.html',
   styleUrls: ['./forums.component.css']
 })
 export class ForumsComponent {
+  forums:Forums[] = forumsData;
 
 }
